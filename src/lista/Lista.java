c49e240bb79f53a34ed2e5ccb7d38b7e530c55b5
Valdir Luiz Hofer <valdir@substractum.com.br>
2015-08-27 17:28:22 -0300

    iniciar implementacao da lista

---
+++ src/lista/Lista.java
@@ -0,0 +1,13 @@
+package lista;
+
+/**
+ * Created by valdir on 27/08/15.
+ */
+public class Lista<T> {
+
+    private Elemento<T> primeiroElemento;
+    private Elemento<T> ultimoElemento;
+
+
+
+}
